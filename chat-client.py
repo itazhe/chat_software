a8875a416c9d526a595becf8e7f76b810d8428b0
azhe <602661651@qq.com>
2019-09-05 23:20:59 +0800

    增加了登录和注册页面

--- chat-client.py
+++ chat-client.py
@@ -1,12 +1,120 @@
-#!/usr/bin/python3
-# -*- coding: utf -*-
-
-
-import tkinter as tk
-import tkinter.messagebox
+from tkinter import *
+# import tkinter.messagebox
 import socket, threading
 
+def main_page():
+    '''
+    函数内容：聊天登录注册主页面
+
+    '''
+    # 创建tk对象
+    mychat = Tk()
+
+    # 设置窗口标题
+    mychat.title("Mychat")
+
+    # 设置窗口大小
+    mychat.minsize(400, 300)
+
+    # 设置窗口能否变长变宽，默认为True
+    mychat.resizable(width=False, height=False)
+
+    # 设置用户名标签
+    l1 = Label(mychat, text="用户名：", font=("Arial", 12)).place(x = 40, y = 100)
+    # 设置用户名框，以明文的形式显示
+    e1 = Entry(mychat, show=None, font=(14)).place(x = 115, y = 100)
+
+    # 设置密码标签
+    l2 = Label(mychat, text="密    码：", font=("Arial", 12)).place(x = 40, y = 150)
+    # 设置密码框，以明文的形式显示
+    e2 = Entry(mychat, show="*", font=(14)).place(x = 115, y = 150)
+
+    # 设置登录按钮
+    b = Button(mychat, text="登 录", font=("Arial", 12)).place(x = 120, y = 200)
+
+    b = Button(mychat, text="注 册", font=("Arial", 12)).place(x = 200, y = 200)
+
+
+    mychat.mainloop()
+
+
+def chat_page():
+    '''
+    函数内容：聊天窗口
+    '''
+    # 创建tk对象
+    mychat = Tk()
+    # 设置窗口标题
+    mychat.title("P1901班专属聊天室")
+
+    chat_record_box = Text(mychat)
+    # 窗口不可点击
+    chat_record_box.configure(state=DISABLED)
+    chat_record_box.pack(padx=10, pady=10)
+
+    # 设置窗口能否变长变宽，默认为True
+    mychat.resizable(width=False, height=False)
+
+    chat_msg_box = Text(mychat)
+    chat_msg_box.configure(width=65, height=5)
+    chat_msg_box.pack(side=LEFT, padx=10, pady=10)
+
+    # send_msg_btn = tk.Button(mainWnd, text="发送", command=on_send_msg)
+    send_msg_btn = Button(mychat, text="发送")
+    send_msg_btn.pack(side=RIGHT, padx=10, pady=10, ipadx=15, ipady=15)
+
+    # threading.Thread(target=recv_chat_data)
+
+    mychat.mainloop()
+
+    
+def reg_page():
+    '''
+    函数内容：用户注册页面
+    '''
+    # 创建tk对象
+    mychat = Tk()
+
+    # 设置窗口标题
+    mychat.title("Mychat-Reg")
+
+    # 设置窗口大小
+    mychat.minsize(400, 300)
+
+    # 设置窗口能否变长变宽，默认为True
+    mychat.resizable(width=False, height=False)
+
+    # 设置用户名标签
+    l1 = Label(mychat, text="用户名：", font=("Arial", 12)).place(x = 35, y = 25)
+    # 设置用户名框，以明文的形式显示
+    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 25)
+
+    # 设置密码标签
+    l2 = Label(mychat, text="密    码：", font=("Arial", 12)).place(x = 35, y = 75)
+    # 设置密码框，以明文的形式显示
+    e2 = Entry(mychat, show="*", font=(14)).place(x = 110, y = 75)
+
+    # 设置手机号标签
+    l3 = Label(mychat, text="手机号：", font=("Arial", 12)).place(x = 35, y = 125)
+    # 设置手机号框，以明文的形式显示
+    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 125)
+
+    # 设置邮箱标签
+    l3 = Label(mychat, text="邮    箱：", font=("Arial", 12)).place(x = 35, y = 175)
+    # 设置邮箱框，以明文的形式显示
+    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 175)
+
+    # 设置注册按钮
+    b = Button(mychat, text="注 册", font=("Arial", 12)).place(x = 185, y = 230)
+
+
+    mychat.mainloop()
+
+
 def on_send_msg():
+    '''
+    函数功能：发送聊天消息
+    '''
     nick_name = "azhe"
     chat_msg = chat_msg_box.get(1.0, "end")
     if chat_msg == "\n":
@@ -18,14 +126,18 @@ def on_send_msg():
     try:
         sock.send(data_len + chat_data)
     except:
-        tk.messagebox.showerror("温馨提示", "发送消息失败，请检查网络连接！")
+        messagebox.showerror("温馨提示", "发送消息失败，请检查网络连接！")
     else:
         chat_msg_box.delete(1.0, "end")
         chat_record_box.configure(state=tk.NORMAL)
         chat_record_box.insert("end", chat_data.decode() + "\n")
         chat_record_box.configure(state=tk.DISABLED)
 
+
 def recv_chat_data():
+    '''
+    函数功能：接收聊天消息
+    '''
     while True:
         try:
             while True:
@@ -45,85 +157,23 @@ def recv_chat_data():
                     recv_size += len(tmp_data)
                 else:
                     # 显示
-                    chat_record_box.configure(state=tk.NORMAL)
+                    chat_record_box.configure(state=NORMAL)
                     chat_record_box.insert("end", chat_data.decode() + "\n")
-                    chat_record_box.configure(state=tk.DISABLED)
+                    chat_record_box.configure(state=DISABLED)
                     continue
                 break
         finally:
             global sock
             sock.close()
             sock = socket.socket()
-            sock.connect(("itmojun.com", 9999))
-
+            sock.connect(("azhe.net.cn", 9999))
 
 sock = socket.socket()
-sock.connect(("itmojun.com", 9999))
-
-mainWnd = tk.Tk()
-mainWnd.title("P1901班专属聊天室")
-
-chat_record_box = tk.Text(mainWnd)
-chat_record_box.configure(state=tk.DISABLED)
-chat_record_box.pack(padx=10, pady=10)
-
-chat_msg_box = tk.Text(mainWnd)
-chat_msg_box.configure(width=65, height=5)
-chat_msg_box.pack(side=tk.LEFT, padx=10, pady=10)
-
-send_msg_btn = tk.Button(mainWnd, text="发送", command=on_send_msg)
-send_msg_btn.pack(side=tk.RIGHT, padx=10, pady=10, ipadx=15, ipady=15)
+sock.connect(("azhe.net.cn", 9999))
 
 threading.Thread(target=recv_chat_data)
 
-mainWnd.mainloop()
-
-
-sock.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# if __name__ == "__main__":
+#     main_page()
+#     chat_page()
+#     reg_page()
