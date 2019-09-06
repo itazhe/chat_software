a2ddd5946d6f7f587b209035737d3a4b8e697fe1
azhe <602661651@qq.com>
2019-09-06 16:05:13 +0800

    增加了发送消息的功能

--- chat-client.py
+++ chat-client.py
@@ -30,9 +30,9 @@ def main_page():
     e2 = Entry(mychat, show="*", font=(14)).place(x = 115, y = 150)
 
     # 设置登录按钮
-    b = Button(mychat, text="登 录", font=("Arial", 12)).place(x = 120, y = 200)
+    b = Button(mychat, text="登 录", font=("Arial", 12), command=chat_page).place(x = 120, y = 200)
 
-    b = Button(mychat, text="注 册", font=("Arial", 12)).place(x = 200, y = 200)
+    b = Button(mychat, text="注 册", font=("Arial", 12), command=reg_page).place(x = 200, y = 200)
 
 
     mychat.mainloop()
@@ -42,6 +42,9 @@ def chat_page():
     '''
     函数内容：聊天窗口
     '''
+    global chat_msg_box
+    global chat_record_box
+
     # 创建tk对象
     mychat = Tk()
     # 设置窗口标题
@@ -59,11 +62,11 @@ def chat_page():
     chat_msg_box.configure(width=65, height=5)
     chat_msg_box.pack(side=LEFT, padx=10, pady=10)
 
-    # send_msg_btn = tk.Button(mainWnd, text="发送", command=on_send_msg)
-    send_msg_btn = Button(mychat, text="发送")
+    send_msg_btn = Button(mychat, text="发送", command=on_send_msg)
+    # send_msg_btn = Button(mychat, text="发送")
     send_msg_btn.pack(side=RIGHT, padx=10, pady=10, ipadx=15, ipady=15)
 
-    # threading.Thread(target=recv_chat_data)
+    threading.Thread(target=recv_chat_data).start()
 
     mychat.mainloop()
 
@@ -129,51 +132,53 @@ def on_send_msg():
         messagebox.showerror("温馨提示", "发送消息失败，请检查网络连接！")
     else:
         chat_msg_box.delete(1.0, "end")
-        chat_record_box.configure(state=tk.NORMAL)
+        chat_record_box.configure(state=NORMAL)
         chat_record_box.insert("end", chat_data.decode() + "\n")
-        chat_record_box.configure(state=tk.DISABLED)
+        chat_record_box.configure(state=DISABLED)
 
 
 def recv_chat_data():
     '''
     函数功能：接收聊天消息
     '''
+    global sock
+
     while True:
         try:
             while True:
                 msg_len_data = sock.recv(15)
-
-                if msg_len == 0:
+                if not msg_len_data:
                     break
-                
+
                 msg_len = int(msg_len_data.decode().rstrip())
                 recv_size = 0
-                msg_content_data = b''
+                msg_content_data = b""
                 while recv_size < msg_len:
-                    tmp += sock.recv(msg_len - recv_size)
-                    if tmp == 0:
+                    tmp_data = sock.recv(msg_len - recv_size)
+                    if not tmp_data:
                         break
-                    msg_data += tmp
+                    msg_content_data += tmp_data
                     recv_size += len(tmp_data)
                 else:
                     # 显示
                     chat_record_box.configure(state=NORMAL)
-                    chat_record_box.insert("end", chat_data.decode() + "\n")
+                    chat_record_box.insert("end", msg_content_data.decode() + "\n")
                     chat_record_box.configure(state=DISABLED)
                     continue
                 break
         finally:
-            global sock
-            sock.close()
-            sock = socket.socket()
-            sock.connect(("azhe.net.cn", 9999))
+                sock.close()
+                sock = socket.socket()
+                sock.connect(("127.0.0.1", 9999))
 
 sock = socket.socket()
-sock.connect(("azhe.net.cn", 9999))
+sock.connect(("127.0.0.1", 9999))
 
-threading.Thread(target=recv_chat_data)
+# threading.Thread(target=recv_chat_data).start()
 
-# if __name__ == "__main__":
-#     main_page()
-#     chat_page()
-#     reg_page()
+if __name__ == "__main__":
+    main_page()
+    # chat_page()
+    # reg_page()
+
+sock.close()
