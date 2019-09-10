aa47d22367058691c6d6f69fa1aa1f4f17f9cea5
azhe <602661651@qq.com>
2019-09-10 23:11:14 +0800

    服务器客户端都增加了登录注册校验，客户端增加了注册失败界面

--- chat-client.py
+++ chat-client.py
@@ -7,6 +7,9 @@ def main_page():
     函数内容：聊天登录注册主页面
 
     '''
+    global e1
+    global e2
+
     # 创建tk对象
     mychat = Tk()
 
@@ -30,9 +33,9 @@ def main_page():
     e2 = Entry(mychat, show="*", font=(14)).place(x = 115, y = 150)
 
     # 设置登录按钮
-    b = Button(mychat, text="登 录", font=("Arial", 12), command=chat_page).place(x = 120, y = 200)
+    b1 = Button(mychat, text="登 录", font=("Arial", 12)).place(x = 120, y = 200)
 
-    b = Button(mychat, text="注 册", font=("Arial", 12), command=reg_page).place(x = 200, y = 200)
+    b2 = Button(mychat, text="注 册", font=("Arial", 12), command=reg_page).place(x = 200, y = 200)
 
 
     mychat.mainloop()
@@ -75,6 +78,11 @@ def reg_page():
     '''
     函数内容：用户注册页面
     '''
+    global e3
+    global e4
+    global e5
+    global e6
+
     # 创建tk对象
     mychat = Tk()
 
@@ -88,24 +96,24 @@ def reg_page():
     mychat.resizable(width=False, height=False)
 
     # 设置用户名标签
-    l1 = Label(mychat, text="用户名：", font=("Arial", 12)).place(x = 35, y = 25)
+    l3 = Label(mychat, text="用户名：", font=("Arial", 12)).place(x = 35, y = 25)
     # 设置用户名框，以明文的形式显示
-    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 25)
+    e3 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 25)
 
     # 设置密码标签
-    l2 = Label(mychat, text="密    码：", font=("Arial", 12)).place(x = 35, y = 75)
+    l4 = Label(mychat, text="密    码：", font=("Arial", 12)).place(x = 35, y = 75)
     # 设置密码框，以明文的形式显示
-    e2 = Entry(mychat, show="*", font=(14)).place(x = 110, y = 75)
+    e4 = Entry(mychat, show="*", font=(14)).place(x = 110, y = 75)
 
     # 设置手机号标签
-    l3 = Label(mychat, text="手机号：", font=("Arial", 12)).place(x = 35, y = 125)
+    l5 = Label(mychat, text="手机号：", font=("Arial", 12)).place(x = 35, y = 125)
     # 设置手机号框，以明文的形式显示
-    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 125)
+    e5 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 125)
 
     # 设置邮箱标签
-    l3 = Label(mychat, text="邮    箱：", font=("Arial", 12)).place(x = 35, y = 175)
+    l6 = Label(mychat, text="邮    箱：", font=("Arial", 12)).place(x = 35, y = 175)
     # 设置邮箱框，以明文的形式显示
-    e1 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 175)
+    e6 = Entry(mychat, show=None, font=(14)).place(x = 110, y = 175)
 
     # 设置注册按钮
     b = Button(mychat, text="注 册", font=("Arial", 12)).place(x = 185, y = 230)
@@ -114,11 +122,135 @@ def reg_page():
     mychat.mainloop()
 
 
+def client_login_send():
+    '''
+    函数功能：用户登录请求
+    '''
+    myuname = e1.get(1.0, "end")
+    mypasswd = e2.get(1.0, "end")
+    req = '{"op": 1, "args":{"uname": "myuname", "passwd": "mypasswd"}}'
+    data_top="{:<15}".format(len(req)).encode()
+    sock.send(data_top)
+    sock.send(req.encode())
+
+
+def client_login_recv():
+    '''
+    函数功能：用户登录响应
+    '''
+    data_len = sock.recv(15).decode().rstrip()
+    if len(data_len) > 0:
+        data_len = int(data_len)
+
+        recv_size = 0
+        json_data = b""
+        while recv_size < data_len:
+            tmp = sock.recv(data_len - recv_size)
+            if tmp == 0:
+                break
+            json_data += tmp
+            recv_size += len(tmp)
+
+        json_data = json_data.decode()
+        rsp = json.loads(json_data)
+        if rsp["error_code"] == 0:
+            # print("登录成功！")
+            chat_page()
+
+
+def client_reg_send():
+    '''
+    函数功能：用户注册请求
+    '''
+    myuname = e3.get(1.0, "end")
+    mypasswd = e4.get(1.0, "end")
+    myphone = e5.get(1.0, "end")
+    myemail = e6.get(1.0, "end")
+    req = '{"op": 2, "args": {"uname": "myuname", "passwd": "mypasswd", "phone": "myphone", "email": "myemail"}}'
+    data_top = "{:<15}".format(len(req)).encode()
+    sock.send(data_top)
+    sock.send(req.encode())
+
+
+def client_reg_recv():
+    '''
+    函数功能：用户注册响应
+    '''
+    data_len = sock.recv(15).decode().rstrip()
+    if len(data_len) > 0:
+        data_len = int(data_len)
+
+        recv_size = 0
+        json_data = b""
+        while recv_size < data_len:
+            tmp = sock.recv(data_len - recv_size)
+            if tmp == 0:
+                break
+            json_data += tmp
+            recv_size += len(tmp)
+
+        json_data = json_data.decode()
+        rsp = json.loads(json_data)
+        if rsp["error_code"] == 0:
+            reg_success_page()
+        else:
+            # print("注册失败！")
+            reg_failed_page()
+
+
+def reg_success_page():
+    '''
+    函数内容：注册成功界面
+    '''
+    # 创建tk对象
+    mychat = Tk()
+
+    # 设置窗口标题
+    mychat.title("注册成功！")
+
+    # 设置窗口大小
+    mychat.minsize(400, 300)
+
+    # 设置窗口能否变长变宽，默认为True
+    mychat.resizable(width=False, height=False)
+
+    # 设置用户名标签
+    l1 = Label(mychat, text="恭喜注册成功!!!", font=("Arial", 18)).place(x = 110, y = 120)
+    
+    b2 = Button(mychat, text="返回登录界面", font=("Arial", 12), command=main_page).place(x = 270, y = 250)
+
+    mychat.mainloop()
+
+
+def reg_failed_page():
+    '''
+    函数内容：注册失败界面
+    '''
+    # 创建tk对象
+    mychat = Tk()
+
+    # 设置窗口标题
+    mychat.title("注册失败！")
+
+    # 设置窗口大小
+    mychat.minsize(400, 300)
+
+    # 设置窗口能否变长变宽，默认为True
+    mychat.resizable(width=False, height=False)
+
+    # 设置用户名标签
+    l1 = Label(mychat, text="注册失败!!!", font=("Arial", 18)).place(x = 110, y = 120)
+    
+    b2 = Button(mychat, text="返回注册界面", font=("Arial", 12), command=reg_page).place(x = 270, y = 250)
+
+    mychat.mainloop()
+
+
 def on_send_msg():
     '''
     函数功能：发送聊天消息
     '''
-    nick_name = "azhe"
+    nick_name = main_page.e1.get(1.0, "end")
     chat_msg = chat_msg_box.get(1.0, "end")
     if chat_msg == "\n":
         return
@@ -171,8 +303,8 @@ def recv_chat_data():
                 sock = socket.socket()
                 sock.connect(("127.0.0.1", 9999))
 
-sock = socket.socket()
-sock.connect(("127.0.0.1", 9999))
+# sock = socket.socket()
+# sock.connect(("127.0.0.1", 9999))
 
 # threading.Thread(target=recv_chat_data).start()
 
@@ -180,5 +312,6 @@ if __name__ == "__main__":
     main_page()
     # chat_page()
     # reg_page()
+    # reg_failed_page()
 
-sock.close()
+# sock.close()
--- chat_server.py
+++ chat_server.py
@@ -3,7 +3,15 @@
 
 import socket
 import threading
+import json
+import pymysql
+import sys,os
+import re
 
+conf = json.load(open(r"C:\Users\POWER\Desktop\项目\chat_software\server_conf.json"))  # 加载配置信息
+# dest_file_abs_path = os.path.abspath(sys.argv[1])
+# dest_file_parent_path = os.path.dirname(dest_file_abs_path)
+# dest_file_name = os.path.basename(dest_file_abs_path)
 
 # UDP打洞
 # 定长包头(15B) + 变长聊天消息(昵称:聊天内容)
@@ -11,47 +19,120 @@ import threading
 def client_chat(sock_conn, client_addr):
     try:
         while True:
-                msg_len_data = sock_conn.recv(15)
-                if not msg_len_data:
-                    break
-
-                msg_len = int(msg_len_data.decode().rstrip())
-                recv_size = 0
-                msg_content_data = b""
-                while recv_size < msg_len:
-                    tmp_data = sock_conn.recv(msg_len - recv_size)
-                    if not tmp_data:
-                        break
-                    msg_content_data += tmp_data
-                    recv_size += len(tmp_data)
-                else:
-                    # 发送给其他所有在线的客户端
-                    for sock_tmp, tmp_addr in client_socks: 
-                        if sock_tmp is not sock_conn:
-                            try:
-                                sock_tmp.send(msg_len_data)
-                                sock_tmp.send(msg_content_data)
-                            except:
-                                client_socks.remove((sock_tmp, tmp_addr))
-                                sock_tmp.close()
-                    continue
+            msg_len_data = sock_conn.recv(15)
+            if not msg_len_data:
                 break
+
+            msg_len = int(msg_len_data.decode().rstrip())
+            recv_size = 0
+            msg_content_data = b""
+            while recv_size < msg_len:
+                tmp_data = sock_conn.recv(msg_len - recv_size)
+                if not tmp_data:
+                    break
+                msg_content_data += tmp_data
+                recv_size += len(tmp_data)
+            else:
+                # 发送给其他所有在线的客户端
+                for sock_tmp, tmp_addr in client_socks: 
+                    if sock_tmp is not sock_conn:
+                        try:
+                            sock_tmp.send(msg_len_data)
+                            sock_tmp.send(msg_content_data)
+                        except:
+                            client_socks.remove((sock_tmp, tmp_addr))
+                            sock_tmp.close()
+                continue
+            break
     finally:
             client_socks.remove((sock_conn, client_addr))
             sock_conn.close()
 
+def user_service_thread(sock_conn, client_addr):
+    try:
+        data_len = sock_conn.recv(15).decode().rstrip()
+        if len(data_len) > 0:
+            data_len = int(data_len)
 
-sock_listen = socket.socket()
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock_listen.bind(("127.0.0.1", 9999))
-sock_listen.listen(5)
+            recv_size = 0
+            json_data = b""
+            while recv_size < data_len:
+                tmp = sock_conn.recv(data_len - recv_size)
+                if tmp == 0:
+                    break
+                json_data += tmp
+                recv_size += len(tmp)
+            
+            json_data = json_data.decode()
+            req = json.loads(json_data)
 
-client_socks = []
+            if req["op"] == 1:
+                # 登录校验
+                rsp = {"op": 1, "error_code": 0}
 
-while True:
-    sock_conn, client_addr = sock_listen.accept()
-    client_socks.append((sock_conn, client_addr))
-    threading.Thread(target=client_chat, args=(sock_conn, client_addr)).start()
+                if user_reg_login.check_uname_pwd(req["args"]["uname"], req["args"]["passwd"]):
+                    rsp["error_code"] = 1
+                
+                header_data = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(header_data)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(header_data)
+
+                if not rsp["error_code"]:
+                    client_chat(sock_conn, client_addr)
+
+            elif req["op"] == 2:
+                # 用户注册
+                rsp = {"op": 2, "error_code": 0}
+                if not user_reg_login.user_reg(req["args"]["uname"], req["args"]["passwd"], req["args"]["phone"], req["args"]["email"]):
+                    # 注册失败
+                    rsp["error_code"] = 1
+                else:
+                    # print("用户%s注册成功！" % req["args"]["uname"])
+                    pass
+
+                rsp = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(rsp)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(rsp)            
+
+            elif req["op"] == 3:
+                # 校验用户名是否存在
+                rsp = {"op": 3, "error_code": 0}
+
+                ret = user_reg_login.check_user_name(req["args"]["uname"])
+                if ret == 2:
+                    rsp["error_code"] = 1
+                    # print("校验用户名%s失败！" % req["args"]["uname"])
+                else:
+                    # print("校验用户名%s成功！" % req["args"]["uname"])
+                    pass
+                
+                rsp = json.dumps(rsp).encode()
+                data_len = "{:<15}".format(len(rsp)).encode()
+                sock_conn.send(data_len)
+                sock_conn.send(rsp)            
+    finally:
+        # print("客户端(%s:%s)断开连接！" % client_addr)
+        sock_conn.close()
+
+def main():
+    global client_socks
+
+    sock_listen = socket.socket()
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_listen.bind(("127.0.0.1", 9999))
+    sock_listen.listen(5)
+
+    client_socks = []
+
+    while True:
+        sock_conn, client_addr = sock_listen.accept()
+        client_socks.append((sock_conn, client_addr))
+        threading.Thread(target=client_chat, args=(sock_conn, client_addr)).start()
+
+if __name__ == '__main__':
+    main()
 
 
 
